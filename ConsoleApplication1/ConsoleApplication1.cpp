af5f2d665768a6cb76bc057ae48ebf40af1b0a04
TAPACplay <118310674+TAPACplay@users.noreply.github.com>
2022-12-07 22:44:49 +0200

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    cout << "youre lenght \n";
+    int a;
+    cin >> a;
+    int b;
+    cout << "youre age \n"; 
+    cin >> b;
+    float d;
+    string c;
+    cout << "you man or woman? \n";
+    cin >> c;
+
+    if (c == "man")
+    {
+        d = 50+(a-150) * 0.52 +(b-21)/4;
+    }
+    else {
+       
+        d = 50+(a - 150) * 0.75 + (b - 21) / 5;
+    }
+    cout << d;
+}
